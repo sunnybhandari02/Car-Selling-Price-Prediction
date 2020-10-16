e9de9e16ea287fd842e548191daecea89b6027d9
sunnybhandari02 <sunnybhandari2019@gmail.com>
2020-10-16 13:31:35 +0530

    All files updatetd

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('car_price_model.pkl', 'rb'))
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -20,6 +20,7 @@ def predict():
         Year = int(request.form['Year'])
         Present_Price=float(request.form['Present_Price'])
         Kms_Driven=int(request.form['Kms_Driven'])
+        Kms_Driven2=np.log(Kms_Driven)
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
@@ -27,7 +28,7 @@ def predict():
                 Fuel_Type_Diesel=0
         elif(Fuel_Type_Petrol=='Diesel'):
             Fuel_Type_Petrol==0
-            Fuel_Type_Petrol==1
+            Fuel_Type_Diesel==1
         else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=0
@@ -42,12 +43,12 @@ def predict():
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],2)
+        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        output=round(prediction[0],4)
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at {} Lakhs".format(output))
     else:
         return render_template('index.html')
 
